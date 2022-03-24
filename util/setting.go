babd1151ffa11c2724826215ee6395b7b4f2ed19
StKali <clarkmonkey@163.com>
2022-03-24 18:22:32 +0800

    feat: add database model package

--- util/setting.go
+++ util/setting.go
@@ -3,6 +3,8 @@ package util
 type Setting struct {
 	LogFile string  `mapstructure:"log_file"`
 	LogLevel string `mapstructure:"log_level"`
+	DriverName string `mapstructure:"driver_name"`
+	DriverSourceName string `mapstructure:"driver_source_name"`
 }
 
 var defaultSetting Setting
@@ -10,4 +12,3 @@ var defaultSetting Setting
 func GetSetting() * Setting {
 	return &defaultSetting
 }
-
